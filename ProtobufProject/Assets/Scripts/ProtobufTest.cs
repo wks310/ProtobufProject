7e8ea30c27843dc1adf94da3254f40b278756c94
wks310 <1248894821@qq.com>
2019-08-22 11:33:31 +0800

    Protobuf

---
+++ ProtobufProject/Assets/Scripts/ProtobufTest.cs
@@ -0,0 +1,58 @@
+﻿#region 模块信息
+//=====================================================
+// - Company(公司)：大连创模科技
+// - FileName(文件名):      ProtobufTest.cs
+// - Created(作者):         #AuthorName#
+// - CreateTime(创建时间):  #CreateTime#
+// - Email(邮箱):           #AuthorEmail#
+// - Description(脚本说明):   
+// - Modifier(修改者列表)： 
+// -  
+//======================================================
+#endregion
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using ProtoBuf;
+
+public class ProtobufTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+         // Serialization();
+        NotSerialization();
+
+
+    }
+    /// <summary>
+    /// 序列化User里面的信息内容
+    /// </summary>
+    void Serialization() {
+       
+
+        User user = new User();
+        user.ID = 21;
+        user.UserName = "爱尚游Bin";
+        user.Password = "123456";
+        user.Level = 4;
+        user._UserType = User.UserType.Warrior;
+
+        using (var fs = File.Create(Application.streamingAssetsPath + "/user.bin"))
+        {
+            Serializer.Serialize<User>(fs, user);//把user对象序列化出二进制文件放入fs文件里面
+           
+        }
+    }
+    /// <summary>
+    /// 反序列化User里面的信息内容
+    /// </summary>
+    void NotSerialization() {
+        User user = null;
+        using (var fs=File.OpenRead(Application.streamingAssetsPath+"/user.bin")) {
+            user = Serializer.Deserialize<User>(fs);//反序列化
+        }
+        Debug.Log(string.Format("ID:{0},用户名：{1}，密码：{2}，等级：{3}，角色类型：{4}",user.ID,user.UserName,user.Password,user.Level,user._UserType));
+
+    }
+}
---
+++ ProtobufProject/Assets/Scripts/User.cs
@@ -0,0 +1,34 @@
+﻿#region 模块信息
+//=====================================================
+// - Company(公司)：大连创模科技
+// - FileName(文件名):      user.cs
+// - Created(作者):         #AuthorName#
+// - CreateTime(创建时间):  #CreateTime#
+// - Email(邮箱):           #AuthorEmail#
+// - Description(脚本说明):   
+// - Modifier(修改者列表)： 
+// -  
+//======================================================
+#endregion
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProtoBuf;
+
+[ProtoContract]
+public class User {
+    [ProtoMember(1)]
+    public int ID { get; set; }
+    [ProtoMember(2)]
+    public string UserName { get; set; }
+    [ProtoMember(3)]
+    public string Password { get; set; }
+    [ProtoMember(4)]
+    public int Level { get; set; }
+    [ProtoMember(5)]
+    public UserType _UserType { get; set; }
+    public enum UserType {
+        Master,
+        Warrior
+    }
+}
